7e8f9eb76e9f64de64777be82c552c573abdac97
robertfisahn <146772668+robertfisahn@users.noreply.github.com>
2025-05-31 11:58:30 +0200

    feat: add initial /api/coins endpoint with CoinGecko integration (#1)

---
+++ Mappings/CoinMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CryptoScopeAPI.Models;
+using CryptoScopeAPI.Dtos;
+
+namespace CryptoScopeAPI.Mappings
+{
+    public class CoinMappingProfile : Profile
+    {
+        public CoinMappingProfile()
+        {
+            CreateMap<Coin, CoinListDto>()
+              .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CoinId))
+              .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.ImageUrl))
+              .ForMember(dest => dest.CurrentPrice, opt => opt.MapFrom(src => src.CurrentPriceUsd));
+        }
+    }
+
+}
