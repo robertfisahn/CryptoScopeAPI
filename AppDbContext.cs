88f64147a7bc0447dad4d7452b48ec5eb842b115
robertfisahn <146772668+robertfisahn@users.noreply.github.com>
2025-06-09 20:36:41 +0200

    feat: coin market chart endpoint (#5)

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,10 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using CryptoScopeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoScopeAPI;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Coin> Coins => Set<Coin>();
     public DbSet<SearchCoin> SearchCoins => Set<SearchCoin>();
     public DbSet<CoinDetails> CoinDetails => Set<CoinDetails>();
-
+    public DbSet<CoinMarketChart> CoinMarketCharts => Set<CoinMarketChart>();
 }
